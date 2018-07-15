b139c62b6b6c97f8ccb18053a96176b2ac294f78
ragazzoua <ragazzo@ukr.net>
2018-07-15 21:37:39 +0300

    first commit

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -5,4 +5,58 @@ package com.company;
  */
 
 public class Human {
+
+    private boolean sex;
+    private String name;
+    private String surname;
+    private float height;
+    private float weight;
+
+    public Human(boolean sex, String name, String surname, float height, float weight) {
+        this.sex = sex;
+        this.name = name;
+        this.surname = surname;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
